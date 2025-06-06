22f520897e3e65d63fea858c5b57d374b2a91711
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2025-06-06 05:39:58 +0530

    hhh

--- src/app/components/organism/dashboard/user/navbar.tsx
+++ src/app/components/organism/dashboard/user/navbar.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import DarkModeToggle from "../../../darkModeToggle/darkModeToggle";
 import LogoContainer from "../../../molecule/dashboard/logoContainer";
 import AvatarContainer from "./avatarContainer";
 import Searchbar from "../../../molecule/dashboard/searchBar";
 import { CardDetail } from "../../../../types/CardDetail";
+import Link from "next/link";
 
 const Navbar = ({
   setCardDetails,
@@ -16,7 +16,7 @@ const Navbar = ({
     <header className="backdrop-blur-lg bg-white/60 dark:bg-gray-900/60 border-b border-gray-200 dark:border-gray-800 shadow-xl sticky top-0 z-30 transition-all duration-300">
       <nav className="flex items-center justify-between max-w-7xl mx-auto px-6 py-3">
         {/* Logo */}
-        <a
+        <Link
           href="/"
           className="focus:outline-none hover:scale-105 active:scale-95 transition-transform duration-200 rounded-lg"
           aria-label="Home"
@@ -25,7 +25,7 @@ const Navbar = ({
             logoName="SeaVentures"
             imageUrl="https://w7.pngwing.com/pngs/53/343/png-transparent-red-man-surfing-logo-surfing-investment-banking-surf-the-sea-beach-photography-people.png"
           />
-        </a>
+        </Link>
 
         {/* Searchbar */}
         <div className="flex-1 flex justify-center mx-6">
